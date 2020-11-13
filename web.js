07d8f8a75f7a19fd47d94f2e5bbe706deed8dbb8
Takashi Toyoshima <toyoshim@gmail.com>
2020-11-14 06:49:22 +0900

    disable momolog

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var cors = require('cors');
 var logfmt = require('logfmt');
-var momolog = require('momolog');
+//var momolog = require('momolog');
 
 var app = express();
 app.use(cors());
 app.use(logfmt.requestLogger());
 
-var collection = process.env.DEBUG ? 'log-debug' : 'log';
-momolog.connect(process.env.MONGODB_URI, collection).then(function(logger) {
-  app.use(logger);
+//var collection = process.env.DEBUG ? 'log-debug' : 'log';
+//momolog.connect(process.env.MONGODB_URI, collection).then(function(logger) {
+//  app.use(logger);
 
   app.use(express.static(__dirname));
 
@@ -17,5 +17,5 @@ momolog.connect(process.env.MONGODB_URI, collection).then(function(logger) {
   app.listen(port, function () {
       console.log('Listening on ' + port);
   });
-});
+//});
 
